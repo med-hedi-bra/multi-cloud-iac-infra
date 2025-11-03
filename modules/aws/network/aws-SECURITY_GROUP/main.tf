d64dd64e85eb6a22f76e2731443c195554145a70
Mohamed Hedi Bra <mohamedhedibra8@gmail.com>
2025-11-03 20:52:06 +0100

    fix: allow http connection in security group

--- modules/aws/network/aws-SECURITY_GROUP/main.tf
+++ modules/aws/network/aws-SECURITY_GROUP/main.tf
@@ -12,6 +12,28 @@ resource "aws_security_group" "security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
